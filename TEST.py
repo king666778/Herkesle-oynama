04be4b7c691c58a92c739cfebb3d6cc1789f3040
king666778 <168629519+king666778@users.noreply.github.com>
2024-06-03 11:48:56 +0300

    Create TEST.py

---
+++ TEST.py
@@ -0,0 +1,25 @@
+"""Define a simple example game."""
+
+# ba_meta require api 8
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import bascenev1 as bs
+from bascenev1lib.game.deathmatch import DeathMatchGame
+
+if TYPE_CHECKING:
+    pass
+
+
+# ba_meta export bascenev1.GameActivity
+class MyGame(DeathMatchGame):
+    """My first ballistica game!"""
+
+    name = 'My Game!'
+
+    def on_begin(self) -> None:
+        super().on_begin()
+        bs.screenmessage('KİNG666 HESABINIZ İYİ ÇALIYOR!!!!', color=(0, 1, 0))
+        
